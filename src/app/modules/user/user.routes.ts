64e21c7f229463b0e88517b801bcc8bdd692b0a6
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-04-22 20:53:21 +0600

    multer added

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { userController } from "./user.controller";
 import { userRole } from "@prisma/client";
 import { auth } from "../../middleWare/Auth";
+import { upload } from "../../shared/fileUpload";
 
 const router = Router()
 
-router.post('/', auth(userRole.ADMIN, userRole.SUPER_ADMIN), userController.createAdmin)
+router.post('/', upload.single("file"), auth(userRole.ADMIN, userRole.SUPER_ADMIN), userController.createAdmin)
 
 export const userRouter = router
